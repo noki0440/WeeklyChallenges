523cfaccbaae5fde78bb0f791fbde124f987897d
morganBarnes <noki9551@gmail.com>
2020-01-27 13:33:51 -0600

    Completed all

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -25,27 +25,53 @@ namespace ChallengesWithTestsMark8
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            var add = number1 + number2;
+            return add;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if(number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            var multiply = factor1 * factor2;
+            return multiply;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
-        }
+            //var names = new[] { "John Thomes", "John", "Leigh Ann", "Leigh" };
+
+            //foreach (var name in names)
+            //{
+            //    var message = "Hello," + names + "!";
+            //}
+
+
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
+        }   
+         
+       
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
